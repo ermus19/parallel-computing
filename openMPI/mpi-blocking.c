faa21e5205d31392635b6d29adf8cb5757a9481c
ermus19 <ermus19@hotmail.com>
2017-04-18 19:24:53 +0200

    Fixed example with real matrix

--- openMPI/mpi-blocking.c
+++ openMPI/mpi-blocking.c
@@ -7,9 +7,8 @@
 int main(int argc, char *argv[])
 {
 
-    int size, rank, i, j, from, to, ndat, part, tag, mA[ORDEN * ORDEN], sumaTotal = 0;
-    double total, t0, t1;
-    MPI_Status info;
+    int size, rank, i, j, ndat, sumaTotal = 0;
+    double t0;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
@@ -17,31 +16,7 @@ int main(int argc, char *argv[])
 
     srand(time(NULL));
 
-    //Inicializo matriz
-    for (i = 0; i < ORDEN * ORDEN; i++)
-    {
-        mA[i] = 0;
-    }
-
-    //Inicializo matriz con numeros entre 0 y 10 si soy maestro
-    if (rank == 0)
-    {
-        for (i = 0; i < ORDEN * ORDEN; i++)
-        {
-            mA[i] = rand() % 10;
-        }
-    }
-
-    /*
-    printf("Proceso %d: mA antes de enviar datos: \n", rank);
-    for (i = 0; i < ORDEN * ORDEN; i++)
-    {
-        printf("%d  ", mA[i]);
-    }
-    printf("\n\n");
-    */
-
-    //Calculamos cuantos datos corresponden a cada proceso
+    //Calculamos cuantos datos corresponden a cada proceso e inicializamos el vector
     ndat = (ORDEN * ORDEN) / size;
     int VA[ndat];
 
@@ -50,14 +25,41 @@ int main(int argc, char *argv[])
         VA[i] = 0;
     }
 
-    //Tomamos tiempo inicial
+    //El maestro inicializa la matriz y la divide
     if (rank == 0)
     {
-        t0 = MPI_Wtime();
-    }
+        int mA[ORDEN][ORDEN];
 
-    //Dividimos el vector en partes, y las distribuimos entre los procesos
-    MPI_Scatter(&mA[0], ndat, MPI_INT, &VA, ndat, MPI_INT, 0, MPI_COMM_WORLD);
+        for (i = 0; i < ORDEN; i++)
+        {
+            for (j = 0; j < ORDEN; j++)
+            {
+                mA[i][j] = rand() % 10;
+            }
+        }
+
+        /*printf("Proceso %d: mA antes de enviar datos: \n", rank);
+        for (i = 0; i < ORDEN; i++)
+        {
+            for (j = 0; j < ORDEN; j++)
+            {
+                printf("%d  ", mA[i][j]);
+            }
+        }
+        printf("\n\n");
+        */
+
+        //Tomamos tiempo inicial
+        t0 = MPI_Wtime();
+
+        //Dividimos el vector en partes, y las distribuimos entre los procesos
+        MPI_Scatter(&mA[0], ndat, MPI_INT, &VA, ndat, MPI_INT, 0, MPI_COMM_WORLD);
+    }
+    else
+    {
+        //Los procesos esclavos no envian pero reciben datos de la llamada a MPI_Scatter
+        MPI_Scatter(NULL, ndat, MPI_INT, &VA, ndat, MPI_INT, 0, MPI_COMM_WORLD);
+    }
 
     for (i = 0; i < ndat; i++)
     {
@@ -69,38 +71,47 @@ int main(int argc, char *argv[])
     //todos los procesos conocen la suma del total de elementos de la matriz
     MPI_Allreduce(&sumaTotal, &sumaTotal, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
 
-    
     printf("Proceso %d: valor de suma total: %d\n", rank, sumaTotal);
-    
-    /*
+
+    //printf("Proceso %d: resta de elementos:\n");
     for (i = 0; i < ndat; i++)
     {
-        VA[i] = VA[i] - sumaTotal;
-        printf("%d  ", VA[i]);
+        VA[i] = abs(VA[i] - sumaTotal);
+        //printf("%d ", VA[i]);
     }
-    printf("\n\n");
-    */
-
-    //Recolectamos valores de cada proceso actualizados
-    MPI_Gather(&VA, ndat, MPI_INT, &mA, ndat, MPI_INT, 0, MPI_COMM_WORLD);
+    //printf("\n\n");
 
     //Tomamos tiempo final y calculamos tiempo ejecucion
     if (rank == 0)
     {
+        int mA[ORDEN][ORDEN];
+        double total, t1;
+
+        //Recolectamos valores de cada proceso actualizados
+        MPI_Gather(&VA, ndat, MPI_INT, &mA, ndat, MPI_INT, 0, MPI_COMM_WORLD);
+
+        //Tomamos tiempo final y calculamos tiempo total
         t1 = MPI_Wtime();
         total = (t1 - t0);
 
-        /*
-        printf("Proceso %d: mA despues de recolectar datos: \n", rank);
-        for (i = 0; i < ORDEN * ORDEN; i++)
+        /*printf("Proceso %d: mA despues de recolectar datos: \n", rank);
+        for (i = 0; i < ORDEN; i++)
         {
-            printf("%d  ", mA[i]);
+            for(j = 0; j < ORDEN; j++)
+            {
+                printf("%d  ", mA[i][j]);
+            }
+            
         }
         printf("\n\n");
         */
 
         printf("\nSe ha tardado %f s\n", total);
+    }
+    else
+    {
 
+        MPI_Gather(&VA, ndat, MPI_INT, NULL, ndat, MPI_INT, 0, MPI_COMM_WORLD);
     }
 
     MPI_Finalize();
